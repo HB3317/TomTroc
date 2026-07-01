e0b069008a48118f32aef41c2185066b740f9fdd
hugo <hugo.blancan@outlook.fr>
2026-07-01 21:05:50 +0200

    Ajout de UserManager

--- models/UserManager.php
+++ models/UserManager.php
@@ -1,1 +1,16 @@
 <?php
+class UserManager extends AbstractEntityManager
+{
+    public function getUserById(int $id): ?User
+    {
+        $sql = "SELECT * FROM users WHERE id = :id";
+        $result = $this->db->query($sql, ['id' => $id]);
+        $user = $result->fetch();
+
+        if ($user === false) {
+            return null;
+        }
+
+        return new User($user);
+    }
+}
