8ec2a77d11d9c5e0f8ee2867261cd9b2dd096817
hugo <hugo.blancan@outlook.fr>
2026-07-01 11:10:23 +0200

    Ajout du BookManager

--- models/AbstractEntityManager.php
+++ models/AbstractEntityManager.php
@@ -1,1 +1,10 @@
 <?php
+abstract class AbstractEntityManager
+{
+    protected DBManager $db;
+
+    public function __construct()
+    {
+        $this->db = DBManager::getInstance();
+    }
+}
--- models/BookManager.php
+++ models/BookManager.php
@@ -1,1 +1,72 @@
 <?php
+class BookManager extends AbstractEntityManager
+{
+    public function getAllBooks() : array
+    {
+        $sql = "SELECT * FROM books";
+        $result = $this->db->query($sql);
+        $books = [];
+
+        while ($book = $result->fetch()) {
+            $books[] = new Book($book);
+        }
+        return $books;
+    }
+
+    public function getBookById(int $id) : ?Book
+    {
+        $sql = "SELECT * FROM books WHERE id = :id";
+        $result = $this->db->query($sql, ['id' => $id]);
+        $book = $result->fetch();
+        if ($book) {
+            return new Book($book);
+        }
+        return null;
+    }
+
+    public function addOrUpdateBook(Book $book) : void 
+    {
+        if ($book->getId() == -1) {
+            $this->addBook($book);
+        } else {
+            $this->updateBook($book);
+        }
+    }
+
+    
+    public function addBook(Book $book) : void
+    {
+        $sql = "INSERT INTO books (user_id, title, author, image, description, status) 
+                VALUES (:user_id, :title, :author, :image, :description, :status)";
+        $this->db->query($sql, [
+            'user_id'       => $book->getUserId(),
+            'title'         => $book->getTitle(),
+            'author'        => $book->getAuthor(),
+            'image'         => $book->getImage(),
+            'description'   => $book->getDescription(),
+            'status'        => $book->getStatus()
+        ]);
+    }
+
+    public function updateBook(Book $book) : void
+    {
+        $sql = "UPDATE books 
+                SET user_id = :user_id, title = :title, author = :author, image = :image, description = :description, status = :status 
+                WHERE id = :id";
+        $this->db->query($sql, [
+            'id'            => $book->getId(),
+            'user_id'       => $book->getUserId(),
+            'title'         => $book->getTitle(),
+            'author'        => $book->getAuthor(),
+            'image'         => $book->getImage(),
+            'description'   => $book->getDescription(),
+            'status'        => $book->getStatus()
+        ]);
+    }
+
+    public function deleteBook(int $id) : void
+    {
+        $sql = "DELETE FROM books WHERE id = :id";
+        $this->db->query($sql, ['id' => $id]);
+    }
+}
