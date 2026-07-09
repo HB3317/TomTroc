d0006ca92f353d1ae64d2280f42cdfccc0ab9656
hugo <hugo.blancan@outlook.fr>
2026-07-09 21:40:13 +0200

    CSS de la page myAccout.

--- views/templates/myAccount.php
+++ views/templates/myAccount.php
@@ -3,7 +3,7 @@
     <div class="user-info">
         <div class="infos">
             <div class="image-info">
-                <img src="./assets/images/users/<?= htmlspecialchars($user->getId()); ?>.jpg?v=" alt="Photo de profil">
+                <img class = "user-image" src="./assets/images/users/<?= htmlspecialchars($user->getId()); ?>.jpg?v=" alt="Photo de profil">
                 <button class="change-image-btn" type="button">Modifier</button>
             </div>
             <div class="text-info">
@@ -17,11 +17,11 @@
         <div class="edit-user-form">
             <form action="index.php?action=modifyUser" method="post" class="form-grid">
                 <label for="email">Adresse email</label>
-                <input type="email" name="email" id="email" value="<?= htmlspecialchars($user->getEmail()); ?>" required>
+                <input class="form-input" type="email" name="email" id="email" value="<?= htmlspecialchars($user->getEmail()); ?>" required>
                 <label for="password">Mot de passe</label>
-                <input type="password" name="password" id="password">
+                <input class="form-input" type="password" name="password" id="password">
                 <label for="nickname">Pseudo</label>
-                <input type="text" name="nickname" id="nickname" value="<?= htmlspecialchars($user->getNickname()); ?>" required>
+                <input class="form-input" type="text" name="nickname" id="nickname" value="<?= htmlspecialchars($user->getNickname()); ?>" required>
                 <button type="submit" class="primary-btn">Enregistrer</button>
             </form>
         </div>
@@ -42,13 +42,13 @@
             <tbody>
                 <?php foreach ($userBooks as $book) { ?>
                     <tr>
-                        <td><img src="<?= htmlspecialchars($book->getImage()); ?>" alt="<?= htmlspecialchars($book->getTitle()); ?>"></td>
+                        <td><img class="book-form-image" src="<?= htmlspecialchars($book->getImage()); ?>" alt="<?= htmlspecialchars($book->getTitle()); ?>"></td>
                         <td><?= htmlspecialchars($book->getTitle()); ?></td>
                         <td><?= htmlspecialchars($book->getAuthor()); ?></td>
                         <td><?= htmlspecialchars($book->getDescription()); ?></td>
                         <td class="book-availability">
                             <span class="<?= $book->getStatus() ? 'available' : 'unavailable'; ?>">
-                                <?= $book->getStatus() ? 'Disponible' : 'Non disponible'; ?>
+                                <?= $book->getStatus() ? 'disponible' : 'non dispo.'; ?>
                             </span>
                         </td>
                         <td>
@@ -62,16 +62,14 @@
     </div>
     <div class="modal hidden" id="change-image-modal">
         <div class="modal-content">
-            <h2>Modifier votre photo</h2>
-            <form action="index.php?action=changeUserImage" method="post" enctype="multipart/form-data">
-                <input type="file" name="userImage" accept=".jpg,.jpeg,.png" required>
+            <div class="modal-header">
+                <p class="modal-header-title">Modifier votre photo</p>
+                <button type="button" class="close-modal">X</button>
+            </div>
+            <form class="modal-form" action="index.php?action=changeUserImage" method="post" enctype="multipart/form-data">
+                <input class="input-file" type="file" name="userImage" accept=".jpg,.jpeg,.png" required>
                 <div class="modal-buttons">
-                    <button type="submit" class="primary-btn">
-                        Enregistrer
-                    </button>
-                    <button type="button" class="secondary-btn close-modal">
-                        Annuler
-                    </button>
+                    <button class="modal-save-btn" type="submit" class="save-btn">Enregistrer</button>
                 </div>
             </form>
         </div>
